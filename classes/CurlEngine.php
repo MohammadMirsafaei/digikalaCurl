883346c8f5263e655470c54d0c2597a9c5f55328
Mohammad Mirsafaei <info@mirsafaei.ir>
2019-07-10 12:07:18 +0430

    CurlEngine Updated

---
+++ classes/CurlEngine.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DigikalaCurl;
+
+class CurlEngine {
+    private $Url;
+    private $Body;
+    private $CurlHandler;
+
+    public function __construct(string $url = "https://www.digikala.com/product/dkp-90825")
+    {
+        $this->Url = $url;
+        $this->CurlHandler = curl_init($this->Url);
+        curl_setopt($this->CurlHandler, CURLOPT_VERBOSE, true);
+        curl_setopt($this->CurlHandler, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->CurlHandler, CURLOPT_SSL_VERIFYPEER, false);
+    }
+
+    public function Exec()
+    {
+        $this->Body = curl_exec($this->CurlHandler);
+    }
+
+    public function GetBody() : string 
+    {
+        return $this->Body;
+    }
+}
