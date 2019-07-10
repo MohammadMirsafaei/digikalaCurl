af78c3b631b7384e579220a6c49dc2d13bd242ae
Mohammad Mirsafaei <info@mirsafaei.ir>
2019-07-10 13:42:28 +0430

    ltr fron complete

--- index.php
+++ index.php
@@ -5,27 +5,41 @@ require_once './vendor/autoload.php';
 use DigikalaCurl\CurlEngine;
 use DigikalaCurl\DigikalaPage;
 
-if(isset($_GET['url']))
+if(isset($_GET['url']) && $_GET['url'] != "")
 {
-    $CE = new CurlEngine();
-    $CE->Exec();
-    $DP = new DigikalaPage($CE->GetBody());
-    echo "
-        <html>
-            <head>
-                <title>".$DP->GetProductName()."</title>
-            </head>
-            <body>
-                <h1>".$DP->GetProductName()."</h1>
-                <h4>".$DP->GetProductPrice()."</h4>
-                <img src='".$DP->GetProductImg()."'>
-            </body>
-        </html>
-
-    ";
-
+    $CE = new CurlEngine($_GET['url']);
 }
 else
 {
-
+    $CE = new CurlEngine();
 }
+$CE->Exec();
+$DP = new DigikalaPage($CE->GetBody());
+?>
+
+<html lang="fa">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>اطلاعات محصول <?= $DP->GetProductName();?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="center">
+        <div class="card" style="width: 18rem;">
+            <img src="<?= $DP->GetProductImg();?>" class="card-img-top">
+            <div class="card-body">
+                <h4><?= $DP->GetProductName();?></h4>
+                <button class="btn btn-block btn-primary"><?= $DP->GetProductPrice();?></button>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
